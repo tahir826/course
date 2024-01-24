38d8f65c6982044ba23192e3fe8b4f1fe6aa74fe
tahir826 <mtahirhassan826@gmail.com>
2024-01-24 15:37:22 +0500

    Calculator

---
+++ calculator/index.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let nums = [{
+        name: 'num1',
+        type: 'number',
+        message: chalk.red.bold.italic('enter your first number'),
+    },
+    {
+        name: 'num2',
+        type: 'number',
+        message: chalk.blue.bold.italic('enter your second number')
+    },
+    {
+        name: 'operators',
+        type: 'list',
+        message: 'select operator',
+        choices: ['+', '*', '/', '-']
+    }
+];
+let operation = inquirer.prompt(nums);
+operation.then((operation) => {
+    switch (operation.operators) {
+        case "*":
+            console.log(chalk.green.bold.italic.bgCyan(`${operation.num1}*${operation.num2} = ${operation.num1 * operation.num2}`));
+            break;
+        case "+":
+            console.log(chalk.yellow.bold.italic.bgBlue(`${operation.num1}+${operation.num2} = ${operation.num1 + operation.num2}`));
+            break;
+        case "-":
+            console.log(chalk.white.bold.italic.bgGreen(`${operation.num1}-${operation.num2} = ${operation.num1 - operation.num2}`));
+            break;
+        case "/":
+            console.log(chalk.cyan.bold.italic.bgWhite(`${operation.num1}/${operation.num2} = ${operation.num1 / operation.num2}`));
+            break;
+        default:
+            break;
+    }
+});
